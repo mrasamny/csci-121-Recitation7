3f924b42ecc4ac6f1ae8898cceddb006ca1ed29c
mrasamny <mrasamny@desu.edu>
2021-03-27 13:42:28 -0400

    Initial commit

---
+++ src/main/java/SkillBuilder7.java
@@ -0,0 +1,34 @@
+/**
+ * SkillBuilder6 is a class for completing the Skill
+ * Builder 6 assignment in Java.
+ *
+ * @author <You>
+ * @version 1.0
+ */
+
+public class SkillBuilder7
+{
+    // define your constants after this comment
+
+
+    public static String leftRightTriangle(int height)
+    {
+        String str = "";
+        //add your code here
+        return str;
+    }
+
+    public static String rightRightTriangle(int height)
+    {
+        String str = "";
+        //add your code here
+        return str;
+    }
+
+    public static String circle(int radius)
+    {
+        String str = "";
+        //add your code here
+        return str;
+    }
+}
